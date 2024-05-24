02678815e71f94141b4c2d3ab3acf1f76f09642a
VitorNoe <162385647+VitorNoe@users.noreply.github.com>
2024-05-24 00:06:33 +0000

    Exerício de Refatoração de Código

---
+++ programa1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+void helloFunction() {
+    std::cout << "Olá mundo." << std::endl;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+void printVector(const std::vector<int>& vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int calculateFactorial(int n) {
+    if (n <= 1) return 1;
+    int result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result = multiply(result, i);
+    }
+    return result;
+}
+
+int main() {
+    int number;
+    std::vector<int> arrayVector = {1, 2, 3, 4, 5};
+    std::cout << "Entre um número: ";
+    std::cin >> number;
+    int factorial = calculateFactorial(number);
+    std::cout << "O fatorial de " << number << " é " << factorial << std::endl;
+
+    return 0;
+}
